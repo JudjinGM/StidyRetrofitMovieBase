2114c003a7ee7e9471dc75f5614c78343a693609
JudjinGM <judjingm@gmail.com>
2023-08-03 11:54:09 +0400

    Hided api key

--- app/src/main/java/com/example/stidyretrofitmoviebase/data/network/ImdbApiService.kt
+++ app/src/main/java/com/example/stidyretrofitmoviebase/data/network/ImdbApiService.kt
@@ -1,23 +1,23 @@
 package com.example.stidyretrofitmoviebase.data.network
 
+import com.example.stidyretrofitmoviebase.BuildConfig
 import com.example.stidyretrofitmoviebase.data.dto.imdb.MovieCastResponse
 import com.example.stidyretrofitmoviebase.data.dto.imdb.MovieDetailsResponse
 import com.example.stidyretrofitmoviebase.data.dto.imdb.MoviesSearchResponse
 import com.example.stidyretrofitmoviebase.data.dto.imdb.NamesSearchResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ImdbApiService {
-    @GET("/en/API/SearchMovie/k_zcuw1ytf/{expression}")
+    @GET("/en/API/SearchMovie/${BuildConfig.API_KEY}/{expression}")
     suspend fun searchMovies(@Path("expression") expression: String): MoviesSearchResponse
 
-    @GET("/en/API/Title/k_zcuw1ytf/{movie_id}")
+    @GET("/en/API/Title/${BuildConfig.API_KEY}/{movie_id}")
     suspend fun getMovieDetails(@Path("movie_id") movieId: String): MovieDetailsResponse
 
-    @GET("/en/API/FullCast/k_zcuw1ytf/{movie_id}")
+    @GET("/en/API/FullCast/${BuildConfig.API_KEY}/{movie_id}")
     suspend fun getFullCast(@Path("movie_id") movieId: String): MovieCastResponse
 
-    @GET("/en/API/SearchName/k_zcuw1ytf/{expression}")
+    @GET("/en/API/SearchName/${BuildConfig.API_KEY}/{expression}")
     suspend fun getNames(@Path("expression") nameQuery: String): NamesSearchResponse
 }
